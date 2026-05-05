9a73745129929def6865a8bce6cf20394699da33
muhdkhan <56386233+muhdkhan@users.noreply.github.com>
2026-05-05 18:04:16 +0530

    Pushing solution to Exercise 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -31,24 +31,26 @@ const Total = ({parts}) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
+    const parts = [
+        {
         name: 'Fundamentals of React',
         exercises: 10
-    }
-    const part2 = {
+        },
+        {
         name: 'Using props to pass data',
         exercises: 7
-    }
-    const part3 = {
+        },
+        {
         name: 'State of a component',
         exercises: 14
-    }
+        }
+    ]
 
     return (
         <>
         <Header course = {course} />
-        <Content parts = { [part1, part2, part3] } />
-        <Total parts = { [part1, part2, part3] } />
+        <Content parts = {parts} />
+        <Total parts = {parts} />
         </>
     )
 }
