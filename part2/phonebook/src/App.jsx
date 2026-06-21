d7bcac75e072ce3356cf19d76295c98dc2436e7a
hanzelah <muhdhanzelah@gmail.com>
2026-06-22 02:56:23 +0530

    Submitting solution to Exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,47 @@
 import { useState } from 'react'
 
+const Filter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={(inputValue) => setFilter(inputValue.target.value)} />
+    </div>
+  )
+}
+
+const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, persons, setPersons }) => {
+  return (
+    <form>
+      <div>
+        name: <input value={newName} onChange={(inputValue) => setNewName(inputValue.target.value)} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={(inputValue) => setNewNumber(inputValue.target.value)} />
+      </div>
+      <div>
+        <button type="submit" onClick={(inputValue) => {
+          inputValue.preventDefault()
+          persons.find(person => person.name === newName) ? alert(`${newName} is already added to phonebook`) :
+          setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }))
+          setNewName('')
+          setNewNumber('')
+        }}>add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, filter }) => {
+  return (
+    <div>
+        {persons
+          .filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+          .map((person, key) => (
+            <div key={key}>{person.name} {person.number}</div>
+          ))}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-1234567', id: 1 },
@@ -15,33 +57,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={(inputValue) => setFilter(inputValue.target.value)} />
-      </div>
-      <h2>add a new</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={(inputValue) => setNewName(inputValue.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(inputValue) => setNewNumber(inputValue.target.value)} />
-        </div>
-        <div>
-          <button type="submit" onClick={(inputValue) => {
-            inputValue.preventDefault()
-            persons.find(person => person.name === newName) ? alert(`${newName} is already added to phonebook`) :
-            setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }))
-            setNewName('')
-            setNewNumber('')
-          }}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        {persons
-          .filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
-          .map((person, key) => (
-            <div key={key}>{person.name} {person.number}</div>
-          ))}
+      <Filter filter={filter} setFilter={setFilter} />
+      <h3>add a new</h3>
+      <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} persons={persons} setPersons={setPersons} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={filter} />
     </div>
   )
 }
